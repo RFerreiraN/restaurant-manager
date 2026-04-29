755c4af06ae552f6b54fb75555170b711422e6ad
RFerreiraN <ricardevferreira@gmail.com>
2026-04-29 22:14:54 +0200

    empezamos a trabajar el componente waiter

--- restaurant-manager-frontend/src/app/app-routing.module.ts
+++ restaurant-manager-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KitchenComponent } from './features/kitchen/kitchen.component';
+import { WaiterComponent } from './features/waiter/waiter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cocina', component: KitchenComponent },
+  { path: 'sala', component: WaiterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- restaurant-manager-frontend/src/app/features/waiter/waiter.component.ts
+++ restaurant-manager-frontend/src/app/features/waiter/waiter.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order.model';
 
 @Component({
   selector: 'app-waiter',
   templateUrl: './waiter.component.html',
   styleUrls: ['./waiter.component.css']
 })
-export class WaiterComponent {
+export class WaiterComponent implements OnInit {
 
+  orders: Order[] = []
+
+  async loadOrders() {
+    const res = await fetch('http://localhost:3000/orders')
+    this.orders = await res.json()
+    console.log(this.orders)
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loadOrders()
+  }
 }
