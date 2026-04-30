549034e7cd5036b518cc8ba0974ae0b1f83b8e7e
RFerreiraN <ricardevferreira@gmail.com>
2026-04-30 21:58:05 +0200

    refact

--- restaurant-manager-frontend/src/app/features/waiter/waiter.component.ts
+++ restaurant-manager-frontend/src/app/features/waiter/waiter.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SOCKET_EVENTS } from 'src/app/core/constants/socket.event';
 import { Order } from 'src/app/core/models/order.model';
+import { SocketService } from 'src/app/core/services/socket.service';
 
 @Component({
   selector: 'app-waiter',
   templateUrl: './waiter.component.html',
   styleUrls: ['./waiter.component.css']
 })
-export class WaiterComponent implements OnInit {
+export class WaiterComponent implements OnInit, OnDestroy {
 
   orders: Order[] = []
 
@@ -16,9 +18,32 @@ export class WaiterComponent implements OnInit {
     console.log(this.orders)
   }
 
-  constructor() { }
+  private statusHandler = (updateOrder: Order) => {
+    this.orders = this.orders.map(order => order._id === updateOrder._id ? updateOrder : order)
+    console.log('evento recibido', updateOrder)
+  }
+
+  private newOrderHandler = (order: Order) => {
+    console.log('Pedido recibido:', order)
+    this.orders.unshift(order)
+  }
+
+  private initSocketListeners() {
+    this.socketService.on<Order>(SOCKET_EVENTS.ORDER_NEW, this.newOrderHandler)
+    this.socketService.on<Order>(SOCKET_EVENTS.ORDER_STATUS_CHANGED, this.statusHandler)
+  }
+
+  constructor(private socketService: SocketService) {
+
+  }
 
   ngOnInit(): void {
     this.loadOrders()
+    this.initSocketListeners()
+  }
+
+  ngOnDestroy(): void {
+    this.socketService.off<Order>(SOCKET_EVENTS.ORDER_NEW, this.newOrderHandler)
+    this.socketService.off<Order>(SOCKET_EVENTS.ORDER_STATUS_CHANGED, this.statusHandler)
   }
 }
